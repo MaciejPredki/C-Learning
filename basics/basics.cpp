f6eade72d2a5d816592c8a620a0b324032ded63a
MaciejPredki <maciejpredki@gmail.com>
2020-07-02 21:16:06 +0200

    Exc 25

--- basics/basics.cpp
+++ basics/basics.cpp
@@ -5,7 +5,58 @@
 
 int main()
 {
-	
+	int number;
+
+	printf("Write the month number: \n");
+	scanf_s("%d", &number);
+
+	if (number > 0 && number< 12)
+	{
+		switch (number)
+		{
+		case 1:
+			printf("January\n");
+			break;
+		case 2:
+			printf("February\n");
+			break;
+		case 3:
+			printf("March\n");
+			break;
+		case 4:
+			printf("April\n");
+			break;
+		case 5:
+			printf("May\n");
+			break;
+		case 6:
+			printf("June\n");
+			break;
+		case 7:
+			printf("July\n");
+			break;
+		case 8:
+			printf("August\n");
+			break;
+		case 9:
+			printf("September\n");
+			break;
+		case 10:
+			printf("October\n");
+			break;
+		case 11:
+			printf("November\n");
+			break;
+		case 12:
+			printf("December\n");
+			break;
+		
+		}
+	}
+	else
+	{
+		printf("Wrong month numbers");
+	}
 
 	return 0;
 
